ad5799b053920dfe0c1d4b4f87bf2bb1df714d70
addcx1developer <addcx1developer@gmail.com>
2024-03-17 16:58:42 +0800

    Section 2: JavaScript Refresher - 3. Import and Export

---
+++ Section 2: JavaScript Refresher/3. Import and Export/assets/scripts/util.js
@@ -0,0 +1,4 @@
+// export let apiKey = 'zaCELgL.0imfnc8mheatherVLWwsAawjYr4Rx-Af50DDqtlx';
+export default 'zaCELgL.0imfnc8mheatherVLWwsAawjYr4Rx-Af50DDqtlx';
+export let apiKey = 'zaCELgL.0imfnc8mheatherVLWwsAawjYr4Rx-Af50DDqtlx';
+export let abc = 'abc';
