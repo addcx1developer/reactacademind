5abbc995b39b4b2c1c8d4486d8f39927cae602d6
addcx1developer <addcx1developer@gmail.com>
2024-03-18 20:47:43 +0800

    Section 2: JavaScript Refresher - 10. Destructuring

---
+++ Section 2: JavaScript Refresher/10. Destructuring/assets/scripts/app.js
@@ -0,0 +1,18 @@
+// const [firstName, lastName] = ['Alexis', 'Dela Cruz'];
+
+// const firstName = userNameData[0];
+// const lastName = userNameData[1];
+
+// console.log(firstName);
+// console.log(lastName);
+
+const { name: userName, age } = {
+	name: 'Heather',
+	age: '24',
+};
+
+// const name = user.name;
+// const age = user.age;
+
+console.log(userName);
+console.log(age);
