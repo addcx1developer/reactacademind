bf52fe57a477ff8b91ac6871c5a3cd1ce93a4070
addcx1developer <addcx1developer@gmail.com>
2024-05-15 13:52:15 +0800

    Section 19: Diving into Redux (An Alternative To The Context API) - 12. Connecting Redux Toolkit State

---
+++ Section 19: Diving into Redux (An Alternative To The Context API)/12. Connecting Redux Toolkit State/src/store/index.js
@@ -0,0 +1,28 @@
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+const initialState = { counter: 0, showCounter: true };
+
+const counterSlice = createSlice({
+	name: 'counter',
+	initialState,
+	reducers: {
+		increment(state) {
+			state.counter++;
+		},
+		decrement(state) {
+			state.counter--;
+		},
+		increase(state, action) {
+			state.counter = state.counter + action.amount;
+		},
+		toggleCounter(state) {
+			state.showCounter = !state.showCounter;
+		},
+	},
+});
+
+const store = configureStore({
+	reducer: counterSlice.reducer,
+});
+
+export default store;
