70f9d971cce7ce3d3571534feb05ea430482b203
addcx1developer <addcx1developer@gmail.com>
2024-05-19 18:16:00 +0800

    Section 21: Building a Multi-Page SPA with React Router - 22. Error Handling with Custom Errors

---
+++ Section 21: Building a Multi-Page SPA with React Router/22. Error Handling with Custom Errors/frontend/src/pages/Events.jsx
@@ -0,0 +1,28 @@
+import { useLoaderData } from 'react-router-dom';
+
+import EventsList from '../components/EventsList.jsx';
+
+function EventsPage() {
+	const data = useLoaderData();
+
+	// if (data.isError) {
+	// 	return <p>{data.message}</p>;
+	// }
+
+	const events = data.events;
+
+	return <EventsList events={events} />;
+}
+
+export default EventsPage;
+
+export async function loader() {
+	const response = await fetch('http://localhost:8080/eventssss');
+
+	if (!response.ok) {
+		// return { isError: true, message: 'Could not fetch events.' };
+		throw { message: 'Could not fetch events.' };
+	} else {
+		return response;
+	}
+}
