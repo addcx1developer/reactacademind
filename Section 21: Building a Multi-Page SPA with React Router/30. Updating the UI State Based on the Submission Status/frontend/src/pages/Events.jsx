b19a2508e765ef2a7624f64a68cb356a52de15cd
addcx1developer <addcx1developer@gmail.com>
2024-05-21 15:40:06 +0800

    Section 21: Building a Multi-Page SPA with React Router - 30. Updating the UI State Based on the Submission Status

---
+++ Section 21: Building a Multi-Page SPA with React Router/30. Updating the UI State Based on the Submission Status/frontend/src/pages/Events.jsx
@@ -0,0 +1,23 @@
+import { json, useLoaderData } from 'react-router-dom';
+
+import EventsList from '../components/EventsList.jsx';
+
+function EventsPage() {
+	const data = useLoaderData();
+
+	const events = data.events;
+
+	return <EventsList events={events} />;
+}
+
+export default EventsPage;
+
+export async function loader() {
+	const response = await fetch('http://localhost:8080/events');
+
+	if (!response.ok) {
+		throw json({ message: 'Could not fetch events.' }, { status: 500 });
+	} else {
+		return response;
+	}
+}
