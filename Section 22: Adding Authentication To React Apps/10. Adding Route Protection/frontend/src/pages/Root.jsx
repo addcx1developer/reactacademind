b6caea41e1540afd5445ee15ae098dd00ac35810
addcx1developer <addcx1developer@gmail.com>
2024-05-25 01:49:05 +0800

    Section 22: Adding Authentication To React Apps - 10. Adding Route Protection

---
+++ Section 22: Adding Authentication To React Apps/10. Adding Route Protection/frontend/src/pages/Root.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from 'react-router-dom';
+
+import MainNavigation from '../components/MainNavigation.jsx';
+
+function RootLayout() {
+	// const navigation = useNavigation();
+
+	return (
+		<>
+			<MainNavigation />
+			<main>
+				{/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+				<Outlet />
+			</main>
+		</>
+	);
+}
+
+export default RootLayout;
